4ff8bd3c9a3fcb4a75ef24a85ecd71c9a0f5f595
Aung Kaung Myat <115795473+akm-coding@users.noreply.github.com>
2025-05-24 01:17:03 +0700

    chore: remove comments

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -12,13 +12,10 @@ const Home = () => {
       <HeroSection />
 
       <div className="container mx-auto p-6 md:p-10">
-        {/* Latest News & Updates */}
         <LatestNewsAndUpdates />
 
-        {/* Next Match Countdown */}
         <NextMatch />
 
-        {/* Featured Content / Highlights */}
         <FeaturedContentAndHighlights />
       </div>
     </div>
