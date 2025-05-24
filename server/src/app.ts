3667afb95d07b9f410a0702fdcba9ec9db9745e8
Aung Kaung Myat <115795473+akm-coding@users.noreply.github.com>
2025-05-24 19:11:47 +0700

    -

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,10 +19,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://zmutd-3rvbnwlrg-aungkaungmyats-projects.vercel.app",
-    ],
+    origin: ["http://localhost:5173", "https://zmutd.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
