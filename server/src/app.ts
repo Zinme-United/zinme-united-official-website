c16a4a618ae22bc82abd7b19326ee67a1f0f449e
Aung Kaung Myat <115795473+akm-coding@users.noreply.github.com>
2025-05-24 18:52:18 +0700

    -

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://zmutd.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
