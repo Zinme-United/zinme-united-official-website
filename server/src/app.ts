8385b774e28d4c7ef7efeff851ba4e9f4fa1bdd5
Aung Kaung Myat <115795473+akm-coding@users.noreply.github.com>
2025-08-17 19:57:35 +0700

    cors origin error

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,7 +21,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN_DEV!, process.env.CORS_ORIGIN_RENDER!],
+    origin: [
+      "https://zmutd.vercel.app",
+      "https://zinme-united-official-website.onrender.com",
+    ],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
